071c9c8c2dd590562c284a23a401a07eb5e686d3
cmoyano <cristianmoyano.mza@gmail.com>
2020-01-12 22:36:19 -0300

    Improve search members

--- members/views.py
+++ members/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 from django.contrib.auth.models import User
+from accounts.models import Profile
 from django.db.models import Q
 from functools import reduce
 
@@ -14,6 +15,7 @@ class MembersView(ListView):
     model = User
     paginate_by = 6  # if pagination is desired
     context_object_name = 'members'
+    valid_profile_fields = ['first_name', 'last_name', 'dni', 'telephone', 'email']
     # ordering = ['-posted']
 
     def get_context_data(self, **kwargs):
@@ -21,11 +23,22 @@ class MembersView(ListView):
         return context
 
     def get_queryset(self):
-        fields = [m.name for m in self.model._meta.fields]
-        result = super(MembersView, self).get_queryset()
+        user_fields = [m.name for m in self.model._meta.fields]
+        profile_fields = [m.name for m in Profile._meta.fields]
+        profile_searcheable_fields = []
+        for profile_field in profile_fields:
+            if profile_field in self.valid_profile_fields:
+                profile_searcheable_fields.append(profile_field)
+        profiles_queryset = Profile.objects.all()
+        users_queryset = super(MembersView, self).get_queryset()
         query = self.request.GET.get('q')
+        result = users_queryset
         if query:
-            result = result.filter(
-                reduce(lambda x, y: x | Q(**{"{}__icontains".format(y): query}), fields, Q())
+            result = users_queryset.filter(
+                reduce(lambda x, y: x | Q(**{"{}__icontains".format(y): query}), user_fields, Q())
             )
+            if not result:
+                result = profiles_queryset.filter(
+                    reduce(lambda x, y: x | Q(**{"{}__icontains".format(y): query}), profile_searcheable_fields, Q())
+                )
         return result
